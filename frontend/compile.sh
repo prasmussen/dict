07d0abddec42ddb1d66b26195374bda6ad2cf69f
Petter Rasmussen <petter.rasmussen@gmail.com>
2016-03-13 12:20:02 +0100

    Find chrome tab by title

--- frontend/compile.sh
+++ frontend/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 elm make Main.elm --output ../web/dict.js
-chrome-cli reload -t 11895
+chrome-cli reload -t $(chrome-cli list tabs | grep "Dict v7" | tail -n 1 | awk  -F '[^0-9]+' '{print $2}')
