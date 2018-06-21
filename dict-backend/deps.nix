68e36063fe6b9455ccbb96b73bf8b336cabdb4c8
Petter Rasmussen <petter.rasmussen@gmail.com>
2018-06-22 00:06:33 +0200

    Add nixpkg default.nix and deps.nix
    Build with `nix-build -E 'with import <nixpkgs> { }; callPackage ./default.nix {}'`

---
+++ dict-backend/deps.nix
@@ -0,0 +1,20 @@
+[
+  {
+    goPackagePath = "github.com/bmizerany/pat";
+    fetch =
+      { type = "git";
+        url = "https://github.com/bmizerany/pat";
+        rev = "6226ea591a40176dd3ff9cd8eff81ed6ca721a00";
+        sha256 = "0qjkm7169y6pybwh0s02fjjk251isa2b367xqfzrwvl6cy4yzfxp";
+      };
+  }
+  {
+    goPackagePath = "gopkg.in/mgo.v2";
+    fetch =
+      { type = "git";
+        url = "https://gopkg.in/mgo.v2";
+        rev = "3f83fa5005286a7fe593b055f0d7771a7dce4655";
+        sha256 = "19vwb6qlcyh3nh6pkk0bynwmr5cmi6mm4hdz01lwb4ybnkzxryc7";
+      };
+  }
+]
