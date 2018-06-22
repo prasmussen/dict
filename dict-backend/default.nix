1e2f5c60a83d611387cfec20d3bbfe07e82688b6
Petter Rasmussen <petter.rasmussen@gmail.com>
2018-06-22 21:54:01 +0200

    Import pkgs if not given
    This package is not supposed to be added to nixpkgs anyways

--- dict-backend/default.nix
+++ dict-backend/default.nix
@@ -1,13 +1,13 @@
-{ stdenv, buildGoPackage, fetchgit }:
+{ pkgs ? import <nixpkgs> {} }:
 
-buildGoPackage rec {
+pkgs.buildGoPackage rec {
   name = "dict-backend-${version}";
   version = "2018-06-21";
   rev = "51882fe0198a88bb177f2dcd7a7d5f84e03a2256";
 
   goPackagePath = "github.com/prasmussen/dict";
 
-  src = fetchgit {
+  src = pkgs.fetchgit {
     rev = rev;
     url = "https://github.com/prasmussen/dict";
     sha256 = "1yv9ik14m9zi8dvx97nfa6rsnpfw6csdqq0s28268ik9jyr9by2k";
