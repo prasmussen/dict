f39cb816c6c6a3a65569764f78eb3bc74af15789
Petter Rasmussen <petter.rasmussen@gmail.com>
2018-06-23 10:19:26 +0200

    Update hashes

--- dict-backend/default.nix
+++ dict-backend/default.nix
@@ -3,14 +3,14 @@
 pkgs.buildGoPackage rec {
   name = "dict-backend-${version}";
   version = "2018-06-21";
-  rev = "51882fe0198a88bb177f2dcd7a7d5f84e03a2256";
+  rev = "02e898b6b0850ea37046e85bfecb5a84651d395a";
 
   goPackagePath = "github.com/prasmussen/dict";
 
   src = pkgs.fetchgit {
     rev = rev;
     url = "https://github.com/prasmussen/dict";
-    sha256 = "1yv9ik14m9zi8dvx97nfa6rsnpfw6csdqq0s28268ik9jyr9by2k";
+    sha256 = "1al2613znb2898571r1fy1jia7limd6r8vqmxrs2hs2zny5pz8mx";
   };
 
   goDeps = ./deps.nix;
